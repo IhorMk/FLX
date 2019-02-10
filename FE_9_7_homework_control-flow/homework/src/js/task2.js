9bb300b6ec55c0cdf3533153eedb143089ba7b2e
Ihor Blok <ihorblok@gmail.com>
2019-02-11 00:00:55 +0200

    Add files via upload

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -1,1 +1,14 @@
-// Your code goes here
+alert("You have 10 guesses.Guess 1-10");
+
+let answer = 3;
+let guess;
+for (i = 0; i < 10; i++) {
+    guess = prompt("What's your guess?");
+    if (answer === guess) {
+        alert("You guessed corectly");
+        break;
+    } else {
+        guess = prompt("Please try again")
+    }
+
+}
